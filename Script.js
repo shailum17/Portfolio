fa22b8bb5b006fdb6034a48f7bc55be2ced92d46
Shailendra <shailendramourya17@gmail.com>
2024-09-19 11:01:11 +0530

    adding theme styles

--- Script.js
+++ Script.js
@@ -51,3 +51,12 @@ document.addEventListener('DOMContentLoaded', function () {
     typeWriter();
   });
   
+document.addEventListener('DOMContentLoaded', function() {
+  var icon = document.getElementById('icon');
+  icon.onclick = function() {
+    document.body.classList.toggle('dark-theme');
+    if(document.body.classList.contains('dark-theme'))
+      icon.src = "sun.png";
+    else icon.src = "moon.png"
+  };
+})
