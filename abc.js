796e352e9d84f7a6fe990071e99ba76d66432417
Shailendra Mourya <shailendramourya17@gmail.com>
2025-03-02 11:39:22 +0530

    Add Cursor Animation

--- abc.js
+++
@@ -1,6 +0,0 @@
-// Smooth scrolling to the contact section
-document.querySelector('.scroll-down').addEventListener('click', function(event) {
-    event.preventDefault();
-    const contactSection = document.querySelector('#contact');
-    contactSection.scrollIntoView({ behavior: 'smooth' });
-});
